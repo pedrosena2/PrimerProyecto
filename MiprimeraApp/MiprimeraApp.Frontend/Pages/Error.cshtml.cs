82b9302899bbf14ec37b394480aad8da2e662975
Pedro Andres <pandressc@misena.edu.co>
2021-11-05 18:26:01 -0500

    ok

--- MiprimeraApp/MiprimeraApp.Frontend/Pages/Error.cshtml.cs
+++ MiprimeraApp/MiprimeraApp.Frontend/Pages/Error.cshtml.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace MiprimerApp.Fronted.Pages
+namespace MiprimeraApp.Frontend.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
--- MiprimeraApp/MiprimeraApp.Frontend/Startup.cs
+++ MiprimeraApp/MiprimeraApp.Frontend/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace MiprimerApp.Fronted
+namespace MiprimeraApp.Frontend
 {
     public class Startup
     {
